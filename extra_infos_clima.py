e515c4a18d6536e139bf61c678d75fc91153a59d
felippeximenes <felippelpximenes@outlook.com>
2025-04-28 16:11:44 -0300

    importando bibliotecas

---
+++ extra_infos_clima.py
@@ -0,0 +1,5 @@
+import os # Utilizada para manipulação de arquivos e diretórios
+from os.path import join # Juntar paths
+import pandas as pd # Biblioteca para manipulação de dados
+from datetime import datetime, timedelta # Para manipulação de datas e horas
+
